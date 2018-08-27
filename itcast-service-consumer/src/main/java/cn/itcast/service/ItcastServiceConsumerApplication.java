1848d292223dbe13c3a07e907c9809d46bb6bb00
lijie <1277560510@qq.com>
2018-08-28 01:08:51 +0800

    sadf

--- itcast-service-consumer/src/main/java/cn/itcast/service/ItcastServiceConsumerApplication.java
+++ itcast-service-consumer/src/main/java/cn/itcast/service/ItcastServiceConsumerApplication.java
@@ -1,21 +1,22 @@
 package cn.itcast.service;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
-@EnableDiscoveryClient // 开启Eureka客户端
+//@SpringBootApplication
+//@EnableDiscoveryClient // 开启Eureka客户端
+//@EnableCircuitBreaker //开启熔断
+@SpringCloudApplication
+@EnableFeignClients //开启Feign客户端
 public class ItcastServiceConsumerApplication {
 
-    @Bean
+    /*@Bean
+    @LoadBalanced //开启负载均衡
     public RestTemplate restTemplate(){
         //这次我们使用了Okhttp客户端，只需要注入工厂即可
         return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
-    }
+    }*/
 
     public static void main(String[] args) {
         SpringApplication.run(ItcastServiceConsumerApplication.class, args);
--- itcast-service-consumer/src/main/java/cn/itcast/service/dao/UserDao.java
+++ itcast-service-consumer/src/main/java/cn/itcast/service/dao/UserDao.java
@@ -1,32 +1,47 @@
 package cn.itcast.service.dao;
 
 import cn.itcast.service.pojo.User;
+import cn.itcast.service.pojo.UserClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @Component
+//@DefaultProperties(defaultFallback = "queryUserByIdDefaultFallback")
 public class UserDao {
 
-    @Autowired
-    private RestTemplate restTemplate;
+   /* @Autowired
+    private RestTemplate restTemplate;*/
+
+   @Autowired
+   private UserClient userClient;
 
     // eureka客户端，可以获取到eureka中服务的信息
     @Autowired
     private DiscoveryClient discoveryClient;
 
+    //@HystrixCommand    //(fallbackMethod = "queryUserByIdDefaultFallback")
     public User queryUserById(Long id) {
         // 根据服务名称，获取服务实例。有可能是集群，所以是service实例集合
-        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
+        //List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
         // 因为只有一个UserService。所以获取第一个实例
-        ServiceInstance instance = instances.get(0);
+        //ServiceInstance instance = instances.get(0);
         // 获取ip和端口信息，拼接成服务地址
-        String baseUrl = "http://" + instance.getHost() + ":" + instance.getPort() + "/user/" + id;
-        return this.restTemplate.getForObject(baseUrl, User.class);
+        //String baseUrl = "http://" + instance.getHost() + ":" + instance.getPort() + "/user/" + id;
+        //return this.restTemplate.getForObject(baseUrl, User.class);
+
+
+        //修改调用方法，不在手动获取ip和端口，而是直接通过服务名称调用
+        //String baseUrl = "http://user-service/user/";
+        User user = this.userClient.queryUserById(id);
+        return user;
     }
+
+   /* public User queryUserByIdDefaultFallback(){
+        User user = new User();
+        user.setUserName("默认提示：对不起，网络太拥挤了！");
+        return user;
+
+    }*/
 }
 
